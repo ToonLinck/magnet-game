5b0810a0afcdf32ff61e8c1ff9c4e74e299b85fe
ToonLinck <Johanlinck96@gmail.com>
2022-04-30 18:07:37 +0200

    added magnetism

--- Assets/Scripts/Object/ObjectMovement.cs
+++ Assets/Scripts/Object/ObjectMovement.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class ObjectMovement : MonoBehaviour
 {
 
-    public GameObject player;
-    public float thrust;
 
+
+    GameObject player;
+
+    CircleCollider2D cc;
     Rigidbody2D rig;
 
+    bool isMagnet = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        rig = gameObject.GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
 
-        
+        rig = gameObject.GetComponent<Rigidbody2D>();
 
     }
 
@@ -26,9 +29,47 @@ public class ObjectMovement : MonoBehaviour
         
     }
 
-    private void FixedUpdate()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        rig.AddForce(player.transform.position * thrust);
+
+        switch (collision.gameObject.layer)
+        {
+
+            case 6:
+
+                if (!isMagnet)
+                {
+
+                    transform.parent = player.transform;
+
+                    ConvertToMagnet();
+
+                }
+
+                break;
+
+
+        }
+
+  
     }
+
+    void ConvertToMagnet()
+    {
+
+        gameObject.layer = 6;
+
+        cc = gameObject.AddComponent(typeof(CircleCollider2D)) as CircleCollider2D;
+
+        cc.radius = 4;
+        cc.isTrigger = true;
+        cc.usedByEffector = true;
+
+
+        rig.bodyType = RigidbodyType2D.Kinematic;
+
+        isMagnet = true;
+    }
+
 
 }
