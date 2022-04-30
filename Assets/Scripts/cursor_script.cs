fd6a30acc568801b20008c34b13597c3314bd722
ToonLinck <Johanlinck96@gmail.com>
2022-04-30 11:07:54 +0200

    added first assets ; added cursor and player rotation

---
+++ Assets/Scripts/cursor_script.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursor_script : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public float rotSpeed = 5f;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.position = cursorPos;
+
+        Vector3 vectorToTarget = player.transform.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q,1);
+    }
+}
